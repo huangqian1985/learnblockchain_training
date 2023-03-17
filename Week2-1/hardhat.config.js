bc8cd936c541c1d1d7ee0fa75b360fc10c3ed3e9
huangqian <huangqian1985@qq.com>
2023-03-17 19:18:51 +0800

    部署脚本提交

--- Week2-1/hardhat.config.js
+++ Week2-1/hardhat.config.js
@@ -18,4 +18,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.17",
+
+  networks: {
+    dev: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
+  },
 };
